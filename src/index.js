a71fde9ed11adc1feb0c6db3de545311e96dac4a
Anna <yaroshenkoannette@gmail.com>
2024-07-29 23:48:37 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+ 
+
+   let arrOfDigits = number.toString().split("");
+   return +arrOfDigits.reduceRight(function (previousValue, currentValue, index, array) {
+    return previousValue + currentValue;
+  },)
+
 }
