e865bec684b790a2e2ee41da50a46fd936597013
Anna <yaroshenkoannette@gmail.com>
2024-07-29 23:56:02 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
  
 
-   let arrOfDigits = number.toString().split("");
+   let arrOfDigits = n.toString().split("");
    return +arrOfDigits.reduceRight(function (previousValue, currentValue, index, array) {
     return previousValue + currentValue;
   },)
