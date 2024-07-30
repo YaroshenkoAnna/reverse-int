13f4f2cc36e38535782551e2ab6f8bd31e25d0f3
Anna <yaroshenkoannette@gmail.com>
2024-07-30 08:10:01 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 module.exports = function reverse (n) {
- 
 
-   let arrOfDigits = n.toString().split("");
-   return +arrOfDigits.reduceRight(function (previousValue, currentValue, index, array) {
-    return previousValue + currentValue;
-  },)
+    function reversePositiveNumber(number){
+      let arrOfDigits = number.toString().split("");
+    return +arrOfDigits.reduceRight(function (previousValue, currentValue, index, array) {
+      return previousValue + currentValue;
+    },)
+    }
+
+  if (n < 0) {
+
+    return - reversePositiveNumber(Math.abs(n));
+  } else {
+    return reversePositiveNumber(n);
+  }
+   
 
 }
